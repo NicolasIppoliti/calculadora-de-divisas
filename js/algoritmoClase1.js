fd192ae23a40111a9fcf6c5eab3b23fb0544bdd9
Nicolas Ippoliti <91435048+NicolasIppoliti@users.noreply.github.com>
2022-06-03 10:52:05 -0300

    Delete algoritmoClase1.js

--- js/algoritmoClase1.js
+++
@@ -1,7 +0,0 @@
-let numero1 = Number(prompt('Ingrese un numero mayor a 100.'));
-let numero2 = Number(prompt('Ingrese un numero del 1 al 10.'));
-let resultado = numero1 * numero2;
-
-alert('A continuacion, se multiplicara el primer numero con el segundo.')
-
-alert('El resultado es: ' + resultado);
